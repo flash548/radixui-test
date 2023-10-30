670b4d1c5e1f5370da1bc280d8eb03d5b9817efb
Christopher Zell <czell@revacomm.com>
2023-10-29 20:31:38 -0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import { useEffect } from "react";
+import { useAppState } from "./app-state";
+import { NavBar } from "./nav-bar";
 
 function App() {
+  const appState = useAppState();
+
+  useEffect(() => {
+    document.documentElement.setAttribute("class", appState.themeMode);
+  }, [appState.themeMode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Theme>
+      <NavBar />
+    </Theme>
   );
 }
 
